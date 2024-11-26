091e3afbf59aa393b671c42fc85c1dd5f83ada86
lolothens-e <annacast@espol.edu.ec>
2024-11-25 20:42:42 -0500

    Creacion de repo

---
+++ src/main/java/com/mycompany/magosguerrerospoo7/MagosGuerrerosPOO7.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.magosguerrerospoo7;
+
+/**
+ *
+ * @author Anthony
+ */
+public class MagosGuerrerosPOO7 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
