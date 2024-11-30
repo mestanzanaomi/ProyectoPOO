7f24d3ed34f0e5ca370cfbe93372e91bc73b1359
MoreiraC18 <150203868+MoreiraC18@users.noreply.github.com>
2024-11-29 21:38:22 -0500

    Update Equipo.java

--- src/main/java/com/mycompany/magosguerrerospoo7/Equipo.java
+++ src/main/java/com/mycompany/magosguerrerospoo7/Equipo.java
@@ -14,18 +14,20 @@ public class Equipo {
     private String nombre;
     private ArrayList<Personaje> personajes;
 
-    public Equipo(String nombre) {
-        this.nombre = nombre;
+    public Equipo(String nombre){
+        this.nombre=nombre;
+        this.personajes=new ArrayList<>();
     }
-    
+
     public void agregarPersonaje(Personaje p){
+        
         this.personajes.add(p);
     }
 
     public ArrayList<Personaje> getPersonajes() {
-        return personajes;
+        return this.personajes;
     }
-    
+
     public void atacarOtroEquipo(Equipo e){
         System.out.println("El equipo "+ this.nombre+ " está atacado al equipo "+e);
         for(int i=0;i<this.personajes.size();i++){
@@ -42,8 +44,9 @@ public class Equipo {
                 }
             }
         }
+
+
     }
-    
     public boolean estaDerrotado(){
         for(Personaje p:this.personajes){
             if(p.getVida()>0){
@@ -56,9 +59,12 @@ public class Equipo {
     public String getNombre() {
         return nombre;
     }
+    
+
 
     public String toString() {
         return nombre; 
     }
-    
+
 }
+
