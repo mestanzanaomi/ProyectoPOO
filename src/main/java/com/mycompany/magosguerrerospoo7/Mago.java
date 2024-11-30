505783bd268fc5ef8d78681d8eac7ed8a7f060f5
MoreiraC18 <150203868+MoreiraC18@users.noreply.github.com>
2024-11-29 21:40:37 -0500

    Update Mago.java

--- src/main/java/com/mycompany/magosguerrerospoo7/Mago.java
+++ src/main/java/com/mycompany/magosguerrerospoo7/Mago.java
@@ -3,18 +3,46 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.magosguerrerospoo7;
-
+import java.util.Random;
 /**
  *
  * @author Anthony
  */
-public class Mago extends Personaje{
-    
-    public Mago(String nombre, int vida, int ataque, int defensa) {
-        super(nombre, vida, ataque, defensa);
+public class Mago extends Personaje {
+    private Equipo equipo;
+
+    public Mago(String n, int v, int a, int d, Equipo equipo) {
+        super(n, v, a, d);
+        this.equipo = equipo;
     }
+
     @Override
-    public void usarEstrategia(){
-        
+    public void usarEstrategia() {
+        if (this.equipo == null) {
+            System.out.println(this.getNombre() + " no tiene equipo asignado. No puede usar su estrategia.");
+            System.out.println("ññññññññññññññññññññññññññññññññññññññññ");
+            return;
+        }
+
+        if (equipo.getPersonajes().size() > 1) {
+            Random random = new Random();
+
+            // Se selecciona un personaje del equipo
+            Personaje aliado = null;
+            while (aliado == null || aliado == this) {
+                aliado = equipo.getPersonajes().get(random.nextInt(equipo.getPersonajes().size()));
+            }
+
+            // Se le aumenta la vida al personaje
+            int aumentoVida = (int) (0.25 * this.getVida());
+            aliado.setVida(aliado.getVida() + aumentoVida);
+
+           
+            System.out.println(this.getNombre() + " ha aumentado la vida de " + aliado.getNombre() + " en " + aumentoVida + " puntos.");
+            System.out.println(aliado.getNombre() + " ahora tiene " + aliado.getVida() + " puntos de vida.");
+        } else {
+            System.out.println(this.getNombre() + " no puede usar la estrategia, ya que no hay otros personajes en su equipo.");
+        }
     }
 }
+
